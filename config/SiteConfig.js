1e92fdd5740b801dff4d93306c1dc692e1937edd
shan5742 <asamshan456@gmail.com>
2018-09-10 22:20:07 +0100

    first commit

---
+++ config/SiteConfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Minimal Blog', // Navigation and Site Title
+  siteTitleAlt: 'My Blog', // Alternative Site title for SEO
+  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'Minimal Blog with big typography', // Your site description
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@ITShan88', // Twitter Username - Optional
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Settings for typography.js
+  headerFontFamily: 'Bitter',
+  bodyFontFamily: 'Open Sans',
+  baseFontSize: '18px',
+};
